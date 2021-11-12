0da6bb1a99c985bb823b1b822563debfab9a9608
César Vargas Júnior <cesar_vargas_jr@hotmail.com>
2021-11-11 23:38:09 -0300

    Etapa-2 Curso React

---
+++ src/components/layout/Content.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Content.css'
+import About from '../../views/examples/About'
+import Home from '../../views/examples/Home'
+
+import { Switch, Route } from 'react-router-dom'
+
+
+const Content = props => (
+    <main className="Content">
+        <Switch>
+            <Route path="/about">
+                <About />
+            </Route>
+            <Route path="/">
+                <Home />
+            </Route>
+        </Switch>
+    </main>
+)
+
+export default Content
